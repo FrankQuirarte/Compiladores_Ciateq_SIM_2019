9d21f262834adfed4c1c243d055c2312ef613670
Frank Quirarte <fqp92@hotmail.com>
2019-06-07 12:54:16 -0500

    Tarea 2 added still problems with the #if comparison

---
+++ Tarea 2/class2_preprocessed.c
@@ -0,0 +1,38 @@
+// This is example.h
+#include <stdio.h>
+ 
+void get_number()
+{
+    int this_is_a_number;
+ 
+    printf( "Please enter a number: " );
+    scanf( "%d", &this_is_a_number );
+    printf( "You entered %d", this_is_a_number );
+}
+#define BUFSIZ  1024
+#define EOF (-1)
+
+struct __sFile 
+{
+  int unused;
+}
+// Literally does_not_exists.h does not exists
+
+// What will the preprocessor does in this case?
+
+//#include "does_not_exists.h" 
+
+
+
+
+
+
+
+int main(){
+
+    
+
+// This is a syntax error, how the preprocessor will handle this scenario?
+
+//#include "error.h 
+

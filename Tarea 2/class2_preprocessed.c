5eae90d6de0b9ee7ed14d383173489727107c974
FrankQuirarte92 <fqp92.fq@gmail.com>
2019-06-07 23:19:38 -0500

    working only #include #define #undef removed parts not tested so this matches what was presented on class

--- Tarea 2/class2_preprocessed.c
+++ Tarea 2/class2_preprocessed.c
@@ -9,30 +9,3 @@ void get_number()
     scanf( "%d", &this_is_a_number );
     printf( "You entered %d", this_is_a_number );
 }
-#define BUFSIZ  1024
-#define EOF (-1)
-
-struct __sFile 
-{
-  int unused;
-}
-// Literally does_not_exists.h does not exists
-
-// What will the preprocessor does in this case?
-
-//#include "does_not_exists.h" 
-
-
-
-
-
-
-
-int main(){
-
-    
-
-// This is a syntax error, how the preprocessor will handle this scenario?
-
-//#include "error.h 
-
--- Tarea 2/scratch.py
+++ Tarea 2/scratch.py
@@ -5,12 +5,8 @@ new_file = open("class2_preprocessed.c", "w+")
 #list to save the variables names and values
 defines_list = {}
 #global variables
-DebugMessage = None
-if_reached = False
-else_reached = False
-if_condition = False
-else_condition = False
-
+DebugMessage = True
+#-------------------------------------------------------------------------------------------------
 print("**********Running Frank´s C preprocessor**********************")
 def include_file(file_name):
     try:
@@ -18,45 +14,14 @@ def include_file(file_name):
         file = f.readlines()
         for line in file:
             new_file.write(line)
-        new_file.write("\n")  # Inserts a newline at the end of file'
+        new_file.write("\n")  # Insert newline at the end of file'
         f.close()
     except FileNotFoundError:
         print("Error: Can't find include file \"%s\" " %file_name)
-
-def replace_var(text):
-	for var in defines_list:
-		i = text.find(var)
-		if i > -1: # It was found
-			text = text.replace(var, defines_list[var])
-	return text
-
-def find_var(line):
-    out_of_string = ""
-    new_line = ""
-    inside_of_string = False
-    if "\"" in line:
-        for c in line[:]:
-            if c != "\"":
-                out_of_string = out_of_string + c
-            else:
-                if not inside_of_string:  # First "
-                    inside_of_string = True
-                    out_of_string = replace_var(out_of_string)
-                    new_line = new_line + out_of_string
-                    out_of_string = "\""
-                else:  # Last "
-                    inside_of_string = False
-                    new_line = new_line + out_of_string + "\""
-                    out_of_string = ""
-        if out_of_string:
-            out_of_string = replace_var(out_of_string)
-            new_line = new_line + out_of_string
-    else:
-        new_line = replace_var(line)
-    return new_line
-
+#-------------------------------------------------------------------------------------------------
 with open("class2.c", "r") as fd:
     archivo = fd.readlines()
+
 
 for lineIndex, line in enumerate(archivo, start=1):
 #-------------------------------------------------------------------------------------------------
@@ -65,14 +30,10 @@ for lineIndex, line in enumerate(archivo, start=1):
         if ((line[9] == "<" and line[-1] == ">") or (line[9] == "\"" and line[-1] == "\"")):
             name = ""
             for c in line[10:-1]:
-                if (c != '/' and c != '\\'):
+                if (c != '/') and (c != '\\'):
                     name = name + c
-                else:
-                    if c == '/':
-                        name = name + "//"  # escape slash
-                    else:
-                        name = name + "\\"  # escape slash
-            #if DebugMessage: print("Name: ", name)
+            if DebugMessage:
+                print("Name: ", name)
             include_file(name)  
         else:
             print("Error: Syntax error in #include at line: %d\n" %lineIndex)
@@ -89,7 +50,8 @@ for lineIndex, line in enumerate(archivo, start=1):
         for c in line[charIndex + 1:]:
             value = value + c
         defines_list[var_name] = value
-        #if DebugMessage: print("[%s] = %s" % (var_name, defines_list[var_name]))
+        if DebugMessage:
+            print("[%s] = %s" % (var_name, defines_list[var_name]))
 #-------------------------------------------------------------------------------------------------
     elif line.startswith("#undef "):
         line = line.rstrip()  # removing trailing space
@@ -105,58 +67,16 @@ for lineIndex, line in enumerate(archivo, start=1):
         defines_list[var_name] = value
 #-------------------------------------------------------------------------------------------------
     elif line.startswith("#if "):
-        if_reached = True
-        if_var = ""
-        if_value = ""
-        for lineIndex, c in enumerate(line[4:], start=4):
-            if (c != ' ' and c != '='): # continues until finds white space or simple "="
-                if_var = if_var + c
-            else:
-                break
-        if line[lineIndex + 2] == ' ':
-            lineIndex = lineIndex + 3 #ignores white space
-        else:
-            lineIndex = lineIndex + 2 #no space between "if" and ""==""
-        for c in line[lineIndex:]:
-            if_value = if_value + c #saves if_value to compare with existing defines_list
-
-        try:
-            if defines_list[if_var] == if_value:
-                if_condition = True
-
-        except KeyError:
-            print("Undefined variable: %s at line %d" % (if_var, i))
+        print("#if found TBF.... ")
+        continue
 #-------------------------------------------------------------------------------------------------
     elif line.startswith("#else "):
-        if if_reached:
-            else_reached = True
-            if_reached = False
-        if not if_condition:
-            else_condition = True
-        else:
-            print("Error: No #if detected before line %d" %i)
+        print("#else found TBF.... ")
+        continue
 #-------------------------------------------------------------------------------------------------
     elif line == "#endif":
-        if (if_reached or else_reached):
-            if_reached = False
-            else_reached = False
-#-------------------------------------------------------------------------------------------------
-    elif line.startswith("//") or not line:  # Checks for comments or empty lines
-        new_file.write(line + "\n")
-#-------------------------------------------------------------------------------------------------
-    elif not line.startswith("#"):
-        if (not if_reached and not else_reached):
-            line = find_var(line)
-            new_file.write(line + "\n")
-        else:
-            if (if_reached and if_condition):
-                line = find_var(line)
-                new_file.write(line + "\n")
-            elif (else_reached and else_condition):
-                line = find_var(line)
-                new_file.write(line + "\n")
-            else:
-                continue
+        print("#enfif found TBF.... ")
+        continue
 #-------------------------------------------------------------------------------------------------
 
 fd.close()
